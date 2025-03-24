44a52e363edaa6f404ea103af8612d4901e14934
User <user@example.com>
2025-03-24 13:11:18 -0700

    Update deployment configuration with absolute paths

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/multiplicative_identity/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
 })
