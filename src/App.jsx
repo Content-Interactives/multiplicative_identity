8aba29fadba5ae8510ec575260d4a56e990cc098
User <user@example.com>
2025-03-24 10:47:37 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MultiplicativeIdentity from './components/MultiplicativeIdentity'
+
+function App() {
+  return (
+      <div className="App">
+        <MultiplicativeIdentity />
+      </div>
+  );
+}
+
+export default App;
